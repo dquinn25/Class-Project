f795ac4c90ca7c1912315f761d44b79db7cf125b
David Quinn <david.c.quinn25@gmail.com>
2020-05-30 16:14:43 -0700

    while and for loop

--- ask-name.js
+++ ask-name.js
@@ -2,22 +2,75 @@ addGreeting();
 var username = prompt('what is your name');
 
 document.write('<h3>' + username + '</h3>');
+// var response = prompt('do you know what branch of the military you want to join?');
+// var message ;
 
-
-var response = prompt('do you know what branch of the military you want to join?');
-var message ;
-
-if (response === 'marines'){
-    message = 'scroll down to the 2nd link' ;
-} else if (response === 'army'){
-    message = 'check out the first option' ;
-} else if (response === 'navy'){
-    message = 'scroll down to the 3rd option' ;
-} else if (response === 'airforce'){
-    message = 'scroll down to the last option' ;
-} else { 
-    message = 'Ok here are some options unless you wanted coast guard';
+function pickBranch() {
+    var response = prompt('Do you know what branch you want to join?');
+    while (response !== 'army' && response !== 'navy' && response !== 'airforce' && response !== 'marines')
+        response = prompt('please pick a branch if you are not sure just type army');
+    if (response === 'army')
+        document.write('<h3>' + 'Army Strong!' + '</h3>');
+    if (response === 'marines')
+        document.write('<h3>' + 'Hurah!' + '</h3>');
+    if (response === 'airforce')
+        document.write('<h3>' + 'Aim High!' + '</h3>');
+    if (response === 'navy')
+        document.write('<h3>' + 'Forged By the Sea!' + '</h3>');
 }
+pickBranch();
+
+function howMany(){
+    var count = prompt('How many years will you serve?');
+    while (isNaN(count) || count === ''){
+        count = prompt ('Decide now! this is a decision that will affect the rest of your life....How many years?');
+    }
+    return count;
+}
+var years = howMany();
+
+var star = '<img id="star" src ="star.jpg" alt=""></img>';
+var result = '';
+for (var i = 0; i < years; i++){
+    result = result + star;
+}
+document.write(result);
+
+
+// var message
+// While(message !== 'army'); {
+//     response = prompt('please pick a branch');
+
+    // var answer = ''; 
+    // while (answer !== 'S3kreT P455w0rD') {
+    //   answer = prompt('Enter the passphrase to proceed...');
+    // }
+    
+
+    // function howMany(){
+    //     var count = prompt('how many years will you serve?');
+
+    // function showOrder(){
+    //     var itemType = get getItemType
+    //     var orderTotal = howMany();
+    
+    //     for(var i = 0; i < orderTotal; i++){
+    //         result = result + '<p>' + itemType + '</p>';
+    //     }
+    //     return document.write results;
+    // }
+    
+// if (response === 'marines'){
+//     message = 'scroll down to the 2nd link' ;
+// } else if (response === 'army'){
+//     message = 'check out the first option' ;
+// } else if (response === 'navy'){
+//     message = 'scroll down to the 3rd option' ;
+// } else if (response === 'airforce'){
+//     message = 'scroll down to the last option' ;
+// } else { 
+//     message = 'Ok here are some options unless you wanted coast guard';
+// }
   
 
-document.write('<h2>' + message + '</h2>');
+// document.write('<h2>' + message + '</h2>');
---
+++ test-add.js
@@ -0,0 +1,38 @@
+/*function getItemType(){
+var response = prompt('do you know what branch of the military you want to join?');
+var item;
+
+While(response !== 'amry' && response !== 'marines'){
+    response = prompt('Please answer which branch you want');
+}
+if (response === 'marines'){
+    message = 'scroll down to the 2nd link' ;
+} else if (response === 'army'){
+    message = 'check out the first option' ;
+} else if (response === 'navy'){
+    message = 'scroll down to the 3rd option' ;
+} else if (response === 'airforce'){
+    message = 'scroll down to the last option' ;
+} else { 
+    message = 'Ok here are some options unless you wanted coast guard';
+}
+  
+
+return document.write('<h2>' + message + '</h2>');
+}
+
+    while(isNaN(count) ll count === ''){
+        count = prompt('please enter a number')
+    }
+    return count;
+}
+
+function showOrder(){
+    var itemType = get getItemType
+    var orderTotal = 
+
+    for(var i = 0; i < orderTotal; i++){
+        result = result + '<p>' + itemType + '</p>';
+    }
+    return document.write results;
+}*/
